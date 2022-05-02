bde342a874ffa8a6cfc0de2cf914d4a4e75d0082
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-02 17:05:03 +0500

    Add openingCreateGet & openingCreatePost middleware functions

--- controllers/openings-controller.js
+++ controllers/openings-controller.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const Opening = require('../models/opening');
 require('../models/variation');
 
@@ -23,3 +24,49 @@ exports.openingDetail = (req, res, next) => {
       });
     });
 };
+
+exports.openingCreateGet = (req, res, next) => {
+  res.render('opening-form', {
+    title: 'Add an Opening',
+  });
+};
+
+exports.openingCreatePost = [
+  /* eslint-disable newline-per-chained-call */
+  body('name', 'Name must not be empty.').trim().notEmpty().escape().unescape(),
+  body('moves', 'Moves must not be empty.')
+    .trim()
+    .notEmpty()
+    .escape()
+    .unescape(),
+  body('description').trim().escape().unescape(),
+  body('origin').trim().escape().unescape(),
+  body('chessDotComUrl').trim().escape().unescape,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const opening = new Opening({
+      name: req.body.name,
+      moves: req.body.moves,
+      description: req.body.description,
+      origin: req.body.origin,
+      chessDotComUrl: req.body.chessDotComUrl,
+    });
+
+    if (!errors.isEmpty()) {
+      return res.render('opening-form', {
+        title: 'Add an Opening',
+        opening,
+        errors: errors.array(),
+      });
+    }
+
+    return Opening.findOne({ name: opening.name }).exec((err, result) => {
+      if (err) return next(err);
+      if (result) return res.redirect(result.url);
+      return opening.save((error) => {
+        if (error) return next(error);
+        return res.redirect(opening.url);
+      });
+    });
+  },
+];
--- routes/openings.js
+++ routes/openings.js
@@ -2,11 +2,15 @@ const express = require('express');
 const {
   openingsList,
   openingDetail,
+  openingCreateGet,
+  openingCreatePost,
 } = require('../controllers/openings-controller');
 
 const router = express.Router();
 
 router.get('/', openingsList);
+router.get('/create', openingCreateGet);
+router.post('/create', openingCreatePost);
 router.get('/:id', openingDetail);
 
 module.exports = router;
