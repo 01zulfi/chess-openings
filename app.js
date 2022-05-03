3eec05e366841b38d6b11a13b5b73fa42b7d6cf0
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-03 16:46:01 +0500

    Install & use compression & helmet

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const openingsRouter = require('./routes/openings');
@@ -12,6 +14,8 @@ const variationsRouter = require('./routes/variations');
 const passwordRouter = require('./routes/password');
 
 const app = express();
+
+app.use(helmet());
 
 // Set up mongoose connection
 
@@ -28,6 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
